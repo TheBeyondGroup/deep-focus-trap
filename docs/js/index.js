8643fa7fcf9ab0066ba095ffcd5240c267833fa7
Sean McQuaid <sean@decrypteddesign.com>
2022-05-21 18:16:39 -0700

    feat | add first example

--- docs/js/index.js
+++ docs/js/index.js
@@ -1,1 +1,32 @@
-console.log('Deep Focus Examples')
+import $ from './quatdom'
+import { deepFocusTrap } from '../../src/index'
+
+const modalFocusTrap = new deepFocusTrap({
+  el: '.js-modal',
+  deep: false,
+  escCallback: () => {
+    $('.js-modal-wrapper').hide(true)
+  },
+})
+$().docReady(() => {
+  $('.js-modal-open').on('click', () => {
+    const modal = $('.js-modal').el()
+    const closeBtn = $('.js-modal-close')
+    const overlay = $('.js-modal-wrapper')
+    overlay.show(true)
+    modalFocusTrap.activate()
+    closeBtn.on('click', () => {
+      overlay.hide(true)
+      modalFocusTrap.deactivate()
+    })
+    $(window).on('click', e => {
+      if (
+        !(modal === e.target || modal.contains(e.target)) &&
+        !e.target.className.includes('js-modal-open')
+      ) {
+        overlay.hide(true)
+        modalFocusTrap.deactivate()
+      }
+    })
+  })
+})
